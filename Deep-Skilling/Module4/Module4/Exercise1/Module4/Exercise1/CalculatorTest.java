a157b09eab4f5b53188405ddb54cdd7a03c74e94
Meenakshipati <patipriyasatyameenakshi@gmail.com>
2026-06-24 18:43:02 +0530

    Create CalculatorTest.java

---
+++ Deep-Skilling/Module4/Module4/Exercise1/Module4/Exercise1/CalculatorTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        int result = calc.add(5, 3);
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calc = new Calculator();
+        int result = calc.subtract(5, 3);
+        assertEquals(2, result);
+    }
+}
