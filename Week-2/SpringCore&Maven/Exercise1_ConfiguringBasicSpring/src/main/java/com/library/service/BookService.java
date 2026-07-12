095ebe99719420db20ed0e2052a15172fe6ae7cf
Meenakshipati <patipriyasatyameenakshi@gmail.com>
2026-07-12 19:01:51 +0530

    Add Week-2 Exercise1: Configuring Basic Spring Application

---
+++ Week-2/SpringCore&Maven/Exercise1_ConfiguringBasicSpring/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void getBookServiceDetails() {
+        System.out.println("Service layer calling Repository");
+        bookRepository.getBookDetails();
+    }
+}
