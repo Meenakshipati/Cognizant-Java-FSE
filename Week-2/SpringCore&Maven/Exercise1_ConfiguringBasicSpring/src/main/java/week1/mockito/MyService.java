e47825ec98036cd038191871f33130d2983fe179
Meenakshipati <patipriyasatyameenakshi@gmail.com>
2026-07-12 13:14:21 +0530

    Add Exercise1 with LibraryManagement code

---
+++ Week-2/SpringCore&Maven/Exercise1_ConfiguringBasicSpring/src/main/java/week1/mockito/MyService.java
@@ -0,0 +1,13 @@
+package week1.mockito;
+
+public class MyService {
+    private ExternalApi api;
+    
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+    
+    public String fetchData() {
+        return api.getData();
+    }
+}
